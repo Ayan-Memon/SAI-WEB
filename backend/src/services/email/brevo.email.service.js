99e57b2fdb5709750014b723f95c1e36fc8580af
Ayan Dev <ayangaming0454@gmail.com>
2026-07-06 21:40:26 +0500

    email sending brevo

--- backend/src/services/email/brevo.email.service.js
+++ backend/src/services/email/brevo.email.service.js
@@ -1,18 +1,23 @@
-import * as brevo from '@getbrevo/brevo';
-import { BREVO_API_KEY } from '../../config/config.js';
+import { BrevoClient } from "@getbrevo/brevo";
+import { BREVO_API_KEY } from "../../config/config.js";
 
-const apiInstance = new brevo.TransactionalEmailsApi();
-apiInstance.setApiKey(brevo.TransactionalEmailsApiApiKeys.apiKey, BREVO_API_KEY);
+const brevo = new BrevoClient({
+  apiKey: BREVO_API_KEY,
+});
 
-const sendEmail = async (to, subject, html) => {
+const sendEmail = async (to, subject, text = "", html) => {
   try {
-    const email = new brevo.SendSmtpEmail();
-    email.sender = { email: "ayangaming0454@gmail.com", name: "Sir Adamjee Institute" };
-    email.to = [{ email: to }];
-    email.subject = subject;
-    email.htmlContent = html;
+    const result = await brevo.transactionalEmails.sendTransacEmail({
+      subject,
+      htmlContent: html,
+      textContent: text,
+      sender: {
+        name: "Sir Adamjee Institute",
+        email: "ayangaming0454@gmail.com",
+      },
+      to: [{ email: to }],
+    });
 
-    const result = await apiInstance.sendTransacEmail(email);
     console.log("Email sent:", result);
   } catch (error) {
     console.error("Error sending email:", error);
